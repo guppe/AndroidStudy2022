62d02603348f9806af61b337c6468f45299af66c
guppe <mst39639toi8@gmail.com>
2022-02-08 16:15:56 +0900

    HelloSample

---
+++ HelloSample/HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.websarva.wings.android.hellosample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickButton: Button = findViewById(R.id.clickButton)
+        val listener = HelloListener()
+        clickButton.setOnClickListener(listener)
+    }
+
+    private inner class HelloListener : View.OnClickListener{
+        override fun onClick(v: View?) {
+            val inputName: EditText = findViewById(R.id.nameEdite)
+            val outputText: TextView = findViewById(R.id.outputText)
+            val inputStr = inputName.text.toString()
+
+            outputText.text = inputStr + "さん、こんにちは"
+        }
+    }
+}
