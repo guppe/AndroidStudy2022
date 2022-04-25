90a3565a841e53652d46c702f042d21fc609d877
guppe <mst39639toi8@gmail.com>
2022-04-25 14:01:48 +0900

    ListViewSample2

---
+++ SpringStudy/Green/ListViewSample2/app/src/main/java/com/websarva/wings/android/listviewsample2/OrderConfirmDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.websarva.wings.android.listviewsample2
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import java.lang.IllegalStateException
+
+class OrderConfirmDialogFragment: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(R.string.dialog_title)
+            builder.setMessage(R.string.dialog_msg)
+            builder.setPositiveButton(R.string.btn_ok, DialogButtonClickListener())
+            builder.setNegativeButton(R.string.btn_ng, DialogButtonClickListener())
+            builder.setNeutralButton(R.string.btn_nu, DialogButtonClickListener())
+            builder.create()
+        }
+        return dialog ?: throw IllegalStateException("アクティビティがnullです")
+    }
+
+    private inner class DialogButtonClickListener : DialogInterface.OnClickListener{
+        override fun onClick(dialog: DialogInterface, which: Int) {
+            var msg = ""
+            when(which){
+                DialogInterface.BUTTON_POSITIVE ->
+                    msg = getString(R.string.toast_ok)
+                DialogInterface.BUTTON_NEGATIVE ->
+                    msg = getString(R.string.toast_ng)
+                DialogInterface.BUTTON_NEUTRAL ->
+                    msg = getString(R.string.toast_nu)
+            }
+            Toast.makeText(activity, msg, Toast.LENGTH_LONG).show()
+        }
+    }
+}
